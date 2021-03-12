a7937af08d3a515d517bc3a80df0142d4d832fbb
aalejo92 <76714792+aalejo92@users.noreply.github.com>
2021-03-11 20:11:06 -0500

    Update Address Book.py
    03-11-21 Rearranged user input into a selectable menu, added a string dunder to improve the call back print, and added contact list feature to further develop

--- Address Book.py
+++ Address Book.py
@@ -1,25 +1,53 @@
+# A class to create a person and store data to that specific person
 class person:
-    def __init__(self, first, last, phone, email):
+    def __init__(self, first, last, phone):
         self.first = first
         self.last = last
         self.phone = phone
-        self.email = email
 
+
+
+
+    # function to display/print full name
     def full_name(self):
         print(f'{self.first} {self.last}')
 
-    def full_contact(self):
-        print(f'{self.first} {self.last} - {self.phone} - {self.email}')
+    # converts object to strings
+    def __str__(self):
+        return f"{self.first} {self.last} - {self.phone}"
 
 
-print('Welcome to the address book program\nEnter your contact\'s information')
 
-first_name = input('First name = ')
-last_name = input('Last name = ')
-phone_number = input('Phone number = ')
-email_address = input('Email address = ')
+contacts = list()
 
-print('Your contact information has been stored')
+user_input = ''
+print('Welcome to the address book application')
 
-our_contact = person(first_name, last_name, phone_number, email_address)
-our_contact.full_contact()
+# while loop to give user a list of options
+while user_input != '3':
+    print('\nPlease select an option: '
+          '\n Enter 1 - Create a contact'
+          '\n Enter 2 - Display contacts'
+          '\n Enter 3 - Exit')
+    user_input = input('Enter selection: ')
+
+    # created a conditional check to direct person to desired option
+    if user_input == '1':
+        print('\nEnter your contact\'s information')
+
+        first_name = input('First name = ')
+        last_name = input('Last name = ')
+        phone_number = input('Phone number = ')
+
+        print('Your contact information has been stored')
+
+        our_contact = person(first_name, last_name, phone_number)
+        contacts.append(our_contact)
+
+    elif user_input == '3':
+        print('Goodbye!')
+        break
+
+    elif user_input == '2':
+        for contact in contacts:
+            print(contact)
