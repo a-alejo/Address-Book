31467739234086ff566f3dc76e4c9662217a8095
aalejo92 <76714792+aalejo92@users.noreply.github.com>
2021-03-15 19:53:53 -0400

    Add files via upload
    # 03-15-21 - added a contact search feature, added csv contact storage method

--- Address Book.py
+++ Address Book.py
@@ -1,3 +1,6 @@
+import os
+
+
 # A class to create a person and store data to that specific person
 class person:
     def __init__(self, first, last, phone):
@@ -5,30 +8,36 @@ class person:
         self.last = last
         self.phone = phone
 
-
-
-
     # function to display/print full name
     def full_name(self):
-        print(f'{self.first} {self.last}')
+        return f'{self.first} {self.last}'
 
     # converts object to strings
     def __str__(self):
-        return f"{self.first} {self.last} - {self.phone}"
-
+        return f'{self.first} {self.last} - {self.phone}'
 
 
 contacts = list()
+
+# checks to see if file already exists or needs to be created
+if os.path.isfile('contacts.csv'):
+    with open('contacts.csv') as file:
+        csv_file = file.readlines()
+        for contact_line in csv_file:
+            contact_data = contact_line.rstrip().split(',')
+            contact = person(contact_data[0], contact_data[1], contact_data[2])
+            contacts.append(contact)
 
 user_input = ''
 print('Welcome to the address book application')
 
 # while loop to give user a list of options
-while user_input != '3':
+while user_input != '4':
     print('\nPlease select an option: '
           '\n Enter 1 - Create a contact'
           '\n Enter 2 - Display contacts'
-          '\n Enter 3 - Exit')
+          '\n Enter 3 - Search contacts'
+          '\n Enter 4 - Exit')
     user_input = input('Enter selection: ')
 
     # created a conditional check to direct person to desired option
@@ -44,10 +53,21 @@ while user_input != '3':
         our_contact = person(first_name, last_name, phone_number)
         contacts.append(our_contact)
 
-    elif user_input == '3':
-        print('Goodbye!')
+    elif user_input == '4':
+        print('\nGoodbye!')
         break
 
     elif user_input == '2':
         for contact in contacts:
             print(contact)
+
+    elif user_input == '3':
+        contact_search = input('\n Enter contact\'s name to search: ').capitalize()
+        for contact in contacts:
+            if contact_search in contact.full_name():
+                print(contact)
+
+# creates and adds users contacts into a csv format file
+with open('contacts.csv', 'w') as file:
+    for contact in contacts:
+        file.write(f'{contact.first}, {contact.last}, {contact.phone}\n')
